30db41ed0aca75957d577bc31f014c10324c45df
Alexggmy <119771840+Alexgg1456@users.noreply.github.com>
2025-01-03 13:33:18 -0500

    Create Alexggmy.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Alexggmy.py
@@ -0,0 +1,28 @@
+#https://www.python.org/
+
+#esto es un comentario
+
+"""Esto es un
+comentario de 
+varias lineas"""
+
+'''Este es un comentario 
+de varias 
+lineas'''
+
+#variable
+numero = 0
+mi_perro = "Max"
+
+MY_CONSTANT = "Esto es una constante" #no tiene que modificarse
+
+int = 45
+string = "Hola a todosss"
+float = 5.6
+bool = True
+bool = False
+other_string = 'Hola a todos'
+
+
+
+print("Hola Python")
